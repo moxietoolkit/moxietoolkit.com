1ba854cef41f506d82f5d627af93528707c7fed4
Randy Oest <oest@amazingrando.com>
2025-01-23 09:45:10 -0500

    landing page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const colors = {
+  'grimwild-blue': 'rgba(90, 107, 140, 1)',
+  'grimwild-dark': 'rgba(31, 38, 28, 1)',
+  'grimwild-light': 'rgba(242, 241, 223, 1)',
+  'grimwild-yellow': 'rgba(217, 197, 98, 1)',
+  'grimwild-yellow-dark': 'rgba(191, 167, 94, 1)'
+};
+
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,10 +16,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        ...colors,
       },
     },
   },
   plugins: [],
 };
+
