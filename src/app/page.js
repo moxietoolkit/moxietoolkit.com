f3bbbd6feb906875da69fafc1b222af5831145b8
Randy Oest <oest@amazingrando.com>
2025-01-23 14:34:42 -0500

    update link

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="space-y-4 text-center">
       <Image src={madeWithMoxie} alt="Made with Moxie" className="max-w-96 inline-block" />
       <h1 className="text-3xl font-bold">Moxie toolkit is coming soon.</h1>
-      <p className="text-lg">In the meantime Grimwild's free rules are available at <a href="https://www.drivethrurpg.com/product/399818/Grimwild-Free-Rules?affiliate_id=144937" className="text-grimwild-blue underline">DriveThruRPG</a>.</p>
+      <p className="text-lg">In the meantime Grimwild&apos;s free rules are available at <a href="https://www.drivethrurpg.com/en/product/507201/grimwild-free-edition?affiliate_id=144937" className="text-grimwild-blue underline">DriveThruRPG</a>.</p>
     </div>
   );
 }
