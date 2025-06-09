1a9a630fa251277e8abff72e14be339d6ff7f9ca
Randy Oest <oest@amazingrando.com>
2025-06-08 22:57:26 -0400

    wip

--- src/components/sidebarLayout.js
+++ src/components/sidebarLayout.js
@@ -22,187 +22,67 @@ export default function SidebarLayout({ children, navigation }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <>
-      {/*
-        This example requires updating your template:
+    <div>
+      <Dialog
+        open={sidebarOpen}
+        onClose={setSidebarOpen}
+        className="relative z-50 lg:hidden"
+      >
+        <DialogBackdrop
+          transition
+          className="fixed inset-0 bg-grimwild-green-dark/80 transition-opacity duration-300 ease-linear data-closed:opacity-0"
+        />
 
-        ```
-        <html class="h-full bg-white">
-        <body class="h-full">
-        ```
-      */}
-      <div>
-        <Dialog
-          open={sidebarOpen}
-          onClose={setSidebarOpen}
-          className="relative z-50 lg:hidden"
-        >
-          <DialogBackdrop
+        <div className="fixed inset-0 flex">
+          <DialogPanel
             transition
-            className="fixed inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-closed:opacity-0"
-          />
-
-          <div className="fixed inset-0 flex">
-            <DialogPanel
-              transition
-              className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-closed:-translate-x-full"
-            >
-              <TransitionChild>
-                <div className="absolute top-0 left-full flex w-16 justify-center pt-5 duration-300 ease-in-out data-closed:opacity-0">
-                  <button
-                    type="button"
-                    onClick={() => setSidebarOpen(false)}
-                    className="-m-2.5 p-2.5"
-                  >
-                    <span className="sr-only">Close sidebar</span>
-                    <XMarkIcon
-                      aria-hidden="true"
-                      className="size-6 text-white"
-                    />
-                  </button>
-                </div>
-              </TransitionChild>
-              {/* Sidebar component, swap this element with another sidebar if you like */}
-              <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-4">
-                <div className="flex h-16 shrink-0 items-center">
-                  <img
-                    alt="Your Company"
-                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=white"
-                    className="h-8 w-auto"
-                  />
-                </div>
-                <nav className="flex flex-1 flex-col">
-                  <ul className="flex flex-1 flex-col gap-y-7">
-                    <li>
-                      <ul className="-mx-2 space-y-1">
-                        {/* {navigationData.map((item) => (
-                          <li key={item.name}>
-                            {!item.children ? (
-                              <a
-                                href={item.href}
-                                className={classNames(
-                                  item.current
-                                    ? 'bg-gray-50'
-                                    : 'hover:bg-gray-50',
-                                  'block rounded-md py-2 pr-2 pl-10 text-sm/6 font-semibold text-gray-700',
-                                )}
-                              >
-                                {item.name}
-                              </a>
-                            ) : (
-                              <Disclosure as="div">
-                                <DisclosureButton
-                                  className={classNames(
-                                    item.current
-                                      ? 'bg-gray-50'
-                                      : 'hover:bg-gray-50',
-                                    'group flex w-full items-center gap-x-3 rounded-md p-2 text-left text-sm/6 font-semibold text-gray-700',
-                                  )}
-                                >
-                                  <ChevronRightIcon
-                                    aria-hidden="true"
-                                    className="size-5 shrink-0 text-gray-400 group-data-open:rotate-90 group-data-open:text-gray-500"
-                                  />
-                                  {item.name}
-                                </DisclosureButton>
-                                <DisclosurePanel as="ul" className="mt-1 px-2">
-                                  {item.children.map((subItem) => (
-                                    <li key={subItem.name}>
-                                      <DisclosureButton
-                                        as="a"
-                                        href={subItem.href}
-                                        className={classNames(
-                                          subItem.current
-                                            ? 'bg-gray-50'
-                                            : 'hover:bg-gray-50',
-                                          'block rounded-md py-2 pr-2 pl-9 text-sm/6 text-gray-700',
-                                        )}
-                                      >
-                                        {subItem.name}
-                                      </DisclosureButton>
-                                    </li>
-                                  ))}
-                                </DisclosurePanel>
-                              </Disclosure>
-                            )}
-                          </li>
-                        ))} */}
-                      </ul>
-                    </li>
-                    <li>
-                      <div className="text-xs/6 font-semibold text-indigo-200">
-                        Your teams
-                      </div>
-                      <ul className="-mx-2 mt-2 space-y-1">
-                        {/* {teams.map((team) => (
-                          <li key={team.name}>
-                            <a
-                              href={team.href}
-                              className={classNames(
-                                team.current
-                                  ? 'bg-indigo-700 text-white'
-                                  : 'text-indigo-200 hover:bg-indigo-700 hover:text-white',
-                                'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold',
-                              )}
-                            >
-                              <span className="flex size-6 shrink-0 items-center justify-center rounded-lg border border-indigo-400 bg-indigo-500 text-[0.625rem] font-medium text-white">
-                                {team.initial}
-                              </span>
-                              <span className="truncate">{team.name}</span>
-                            </a>
-                          </li>
-                        ))} */}
-                      </ul>
-                    </li>
-                    <li className="mt-auto">
-                      <a
-                        href="#"
-                        className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-indigo-200 hover:bg-indigo-700 hover:text-white"
-                      >
-                        <Cog6ToothIcon
-                          aria-hidden="true"
-                          className="size-6 shrink-0 text-indigo-200 group-hover:text-white"
-                        />
-                        Settings
-                      </a>
-                    </li>
-                  </ul>
-                </nav>
-              </div>
-            </DialogPanel>
-          </div>
-        </Dialog>
-
-        {/* Static sidebar for desktop */}
-        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-          {/* Sidebar component, swap this element with another sidebar if you like */}
-          <PrimaryNav navigation={navigation} />
-        </div>
-
-        <div className="lg:pl-72">
-          <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-grimwild-green px-4 shadow-xs sm:gap-x-6 sm:px-6 lg:px-8 lg:hidden">
-            <button
-              type="button"
-              onClick={() => setSidebarOpen(true)}
-              className="-m-2.5 p-2.5 text-grimwild-yellow lg:hidden"
-            >
-              <span className="sr-only">Open sidebar</span>
-              <Bars3Icon aria-hidden="true" className="size-8" />
-            </button>
-            <Link href="/">
-              <MadeWithMoxieLogo className="w-[150px] h-auto fill-grimwild-yellow" />
-            </Link>
-          </div>
-
-          <main
-            className="relative py-6 md:py-10 min-h-screen bg-grimwild-light bg-blend-multiply bg-[100%_100%] bg-repeat-y"
-            style={{ backgroundImage: `url(${bgPattern.src})` }}
+            className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-closed:-translate-x-full"
           >
-            <div className="px-8 relative z-10">{children}</div>
-          </main>
+            <TransitionChild>
+              <div className="absolute top-0 left-full flex w-16 justify-center pt-5 duration-300 ease-in-out data-closed:opacity-0">
+                <button
+                  type="button"
+                  onClick={() => setSidebarOpen(false)}
+                  className="-m-2.5 p-2.5"
+                >
+                  <span className="sr-only">Close sidebar</span>
+                  <XMarkIcon aria-hidden="true" className="size-6 text-white" />
+                </button>
+              </div>
+            </TransitionChild>
+
+            <PrimaryNav navigation={navigation} />
+          </DialogPanel>
         </div>
+      </Dialog>
+
+      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+        <PrimaryNav navigation={navigation} />
       </div>
-    </>
+
+      <div className="lg:pl-72">
+        <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-grimwild-green px-4 shadow-xs sm:gap-x-6 sm:px-6 lg:px-8 lg:hidden">
+          <button
+            type="button"
+            onClick={() => setSidebarOpen(true)}
+            className="-m-2.5 p-2.5 text-grimwild-yellow lg:hidden"
+          >
+            <span className="sr-only">Open sidebar</span>
+            <Bars3Icon aria-hidden="true" className="size-8" />
+          </button>
+          <Link href="/">
+            <MadeWithMoxieLogo className="w-[150px] h-auto fill-grimwild-yellow" />
+          </Link>
+        </div>
+
+        <main
+          className="relative py-6 md:py-10 min-h-screen bg-grimwild-light bg-blend-multiply bg-[100%_100%] bg-repeat-y"
+          style={{ backgroundImage: `url(${bgPattern.src})` }}
+        >
+          <div className="px-8 relative z-10">{children}</div>
+        </main>
+      </div>
+    </div>
   );
 }
 
